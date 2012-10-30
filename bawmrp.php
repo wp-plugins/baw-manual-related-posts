61c7c36c3cd7401cc4a0539b937ab2b4f6361671
juliobox <juliobox@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-30 15:44:59 +0000

    1.7.4 !
    git-svn-id: https://plugins.svn.wordpress.org/baw-manual-related-posts/trunk@619062 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bawmrp.php
+++ bawmrp.php
@@ -3,7 +3,7 @@
 Plugin Name: BAW Manual Related Posts
 Plugin URI: http://www.boiteaweb.fr
 Description: Set related posts manually but easily with great ergonomics! Stop displaying auto/random related posts!
-Version: 1.7
+Version: 1.7.4
 Author: Juliobox
 Author URI: http://www.boiteaweb.fr
 */
